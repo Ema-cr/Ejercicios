5cda1486a96179c36a4b16a549ac87f4150668ab
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-04-24 21:18:04 -0500

    Entrenamiento Semana 1.py

--- Entrenamiento Semana 1.py
+++ Entrenamiento Semana 1.py
@@ -23,6 +23,15 @@ solicitar_solo_numeros()
 
 cantidad_productos=int(input("Cuantas unidades deseas?: "))
 precio_total= precio_producto*cantidad_productos
-descuento_producto=float(input("Ingresa el descuento del producto: "))
+descuento_producto=float(input("Ingresa el descuento del producto: "))  #prueba 1
 precio_final= precio_total/descuento_producto
+print(f"El precio del producto es: {precio_final}")
+
+#ARREGLAR
+
+precio_producto=input("Ingresa el precio de tu producto: ")
+cantidad_productos=int(input("Cuantas unidades deseas?: "))
+precio_total=int (cantidad_productos*precio_producto)
+descuento_producto=float(input("Ingresa el descuento del producto: "))  #prueba 2
+precio_final=int (precio_total/descuento_producto)
 print(f"El precio del producto es: {precio_final}")
