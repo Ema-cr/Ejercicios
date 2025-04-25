9dffaa5dfde2a8de947c0100f2d5029121fdf068
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-04-24 21:16:35 -0500

    Add files via upload

---
+++ Entrenamiento Semana 1.py
@@ -0,0 +1,28 @@
+print("Tienda Riwi, Bienvenid@s")
+print("1. Comprar")
+print("2. Salir")
+
+try:
+    Opcion=int(input("Selecciona una opcion: "))
+except ValueError:
+    print("Error, Debes ingresar un numero entero.")
+if Opcion == 1:
+    producto=str (input("Ingresa el producto que deseas comprar: ")) #Arreglar
+    print(f"Elegiste el producto: {producto}")
+elif Opcion == 2:
+    print("Gracias por visitarnos")
+
+def solicitar_solo_numeros():
+    while True:
+precio_producto=input("Ingresa el precio de tu producto: ")
+        if precio_producto.isalnum():
+            print                                 #Arreglar
+            break                                
+        else :print("Ingresa una cantidad valida")
+solicitar_solo_numeros()
+
+cantidad_productos=int(input("Cuantas unidades deseas?: "))
+precio_total= precio_producto*cantidad_productos
+descuento_producto=float(input("Ingresa el descuento del producto: "))
+precio_final= precio_total/descuento_producto
+print(f"El precio del producto es: {precio_final}")
